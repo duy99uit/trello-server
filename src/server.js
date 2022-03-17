8f6b36ad48692df6a8e3f503d8731b977b464543
Duy Dev <duy99.uit@gmail.com>
2022-03-17 14:04:14 +0700

    clean code

--- src/server.js
+++ src/server.js
@@ -1,9 +1,6 @@
-import express from "express";
-import { mapOrder } from "@/utilities/sortArray";
-import { connnectDatabase, listDatabases } from "@/config/mongodb";
 import { env } from "@/config/environtment";
-import { getDB } from "./config/mongodb";
-import { BoardModel } from "@/models/board.model";
+import { connnectDatabase } from "@/config/mongodb";
+import express from "express";
 
 connnectDatabase()
   .then(() => console.log("...Connected to MongoDB Cluster"))
@@ -17,14 +14,9 @@ const bootServer = () => {
   const app = express();
 
   app.get("/", async (req, res) => {
-    let data = {
-      title: "hyyy",
-    };
-
-    const newBoard = await BoardModel.createNew(data);
-    console.log("newBoard", newBoard);
     res.end("<h1>Heyyyyy</h1>");
   });
+
   app.listen(env.APP_PORT, env.APP_HOSTNAME, () => {
     console.log(
       `Server is running at hostname ${env.APP_HOSTNAME}:${env.APP_PORT}/`
