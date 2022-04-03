cea376a5b56d730fcf682bc8475a793b2eb850d1
Duy Dev <duy99.uit@gmail.com>
2022-04-03 20:49:16 +0700

    fix deploy

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -9,5 +9,5 @@ export const HttpStatusCode = {
 export const WHITELIST_DOMAIN = [
   "http://localhost:3000",
   "http://localhost:3001",
-  "https://trello-duyduy.herokuapp.com",
+  "https://trello-99a18.web.app",
 ];
